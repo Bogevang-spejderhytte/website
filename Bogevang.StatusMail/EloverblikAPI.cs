6caa7642be4b2dfe85b5f5f97907800ab2c6aeba
Jorn <jw@elfisk.dk>
2021-06-15 22:42:14 +0200

    Improved error message.

--- Bogevang.StatusMail/EloverblikAPI.cs
+++ Bogevang.StatusMail/EloverblikAPI.cs
@@ -57,60 +57,67 @@ namespace Bogevang.StatusMail.Domain
 
     public async Task<TimeSeries> LoadElectricityTimeSeries(DateTime start, DateTime end)
     {
-      IService elService = RamoneConfiguration.NewService(new Uri(Settings.BaseUrl));
-      ISession elSession = elService.NewSession();
-
-      var tokenRequest = elSession
-        .Bind("api/token")
-        .Header("Authorization", "BEARER " + Settings.AccessToken);
-
-      using (var tokenResponse = await tokenRequest.Async().Get())
+      try
       {
-        dynamic body = tokenResponse.Body;
-        string accessToken = body.result;
+        IService elService = RamoneConfiguration.NewService(new Uri(Settings.BaseUrl));
+        ISession elSession = elService.NewSession();
 
-        // Cannot ask for data in future
-        if (end >= DateTime.Now.Date)
-          end = DateTime.Now.Date.AddDays(-1);
+        var tokenRequest = elSession
+          .Bind("api/token")
+          .Header("Authorization", "BEARER " + Settings.AccessToken);
 
-        var args = new
+        using (var tokenResponse = await tokenRequest.Async().Get())
         {
-          dateFrom = start.ToString("yyyy-MM-dd"),
-          dateTo = end.ToString("yyyy-MM-dd")
-        };
-        var request = elSession
-          .Bind("api/MeterData/GetTimeSeries/{dateFrom}/{dateTo}/Day", args)
-          .Header("Authorization", "BEARER " + accessToken)
-          .AsJson()
-          .AcceptJson();
+          dynamic body = tokenResponse.Body;
+          string accessToken = body.result;
 
-        TimeZoneInfo dkZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+          // Cannot ask for data in future
+          if (end >= DateTime.Now.Date)
+            end = DateTime.Now.Date.AddDays(-1);
 
-        using (var response = await request.Async().Post<StandardResponse<MyEnergyData_MarketDocumentResult>>(new { meteringPoints = new { meteringPoint = new string[] { "571313174111455847" } } }))
-        {
-          var data = response.Body;
-
-          if (data != null && data.result != null && data.result.Length > 0 && data.result[0] != null
-            && data.result[0].MyEnergyData_MarketDocument != null
-            && data.result[0].MyEnergyData_MarketDocument.TimeSeries != null
-            && data.result[0].MyEnergyData_MarketDocument.TimeSeries.Length > 0)
+          var args = new
           {
-            var timeSeries = data.result[0].MyEnergyData_MarketDocument.TimeSeries[0];
+            dateFrom = start.ToString("yyyy-MM-dd"),
+            dateTo = end.ToString("yyyy-MM-dd")
+          };
+          var request = elSession
+            .Bind("api/MeterData/GetTimeSeries/{dateFrom}/{dateTo}/Day", args)
+            .Header("Authorization", "BEARER " + accessToken)
+            .AsJson()
+            .AcceptJson();
 
-            foreach (var entry in timeSeries.Period)
+          TimeZoneInfo dkZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+
+          using (var response = await request.Async().Post<StandardResponse<MyEnergyData_MarketDocumentResult>>(new { meteringPoints = new { meteringPoint = new string[] { "571313174111455847" } } }))
+          {
+            var data = response.Body;
+
+            if (data != null && data.result != null && data.result.Length > 0 && data.result[0] != null
+              && data.result[0].MyEnergyData_MarketDocument != null
+              && data.result[0].MyEnergyData_MarketDocument.TimeSeries != null
+              && data.result[0].MyEnergyData_MarketDocument.TimeSeries.Length > 0)
             {
-              if (entry?.timeInterval?.start != null)
-                entry.timeInterval.start = TimeZoneInfo.ConvertTimeFromUtc(entry.timeInterval.start, dkZone);
+              var timeSeries = data.result[0].MyEnergyData_MarketDocument.TimeSeries[0];
 
-              if (entry?.timeInterval?.end != null)
-                entry.timeInterval.end = TimeZoneInfo.ConvertTimeFromUtc(entry.timeInterval.end, dkZone);
+              foreach (var entry in timeSeries.Period)
+              {
+                if (entry?.timeInterval?.start != null)
+                  entry.timeInterval.start = TimeZoneInfo.ConvertTimeFromUtc(entry.timeInterval.start, dkZone);
+
+                if (entry?.timeInterval?.end != null)
+                  entry.timeInterval.end = TimeZoneInfo.ConvertTimeFromUtc(entry.timeInterval.end, dkZone);
+              }
+
+              return timeSeries;
             }
-
-            return timeSeries;
+            else
+              return null;
           }
-          else
-            return null;
         }
+      }
+      catch (Exception ex)
+      {
+        throw new ApplicationException($"Failed to lookup data from el-overblik API. Message: {ex.Message}");
       }
     }
   }
